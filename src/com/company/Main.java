bc578c4a12fbf0a8e155e64bf674a75ba0234ac9
MadsHougaard <Madshougaard20@gmail.com>
2019-09-17 20:03:26 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,29 @@
+package com.company;
+public class Dog
+{
+    // Instance Variables
+    String name;
+    String breed;
+    int age;
+    String color;
+
+    // Constructor Declaration of Class
+    public Dog(String name, String breed,
+               int age, String color)
+    {
+        this.name = name;
+        this.breed = breed;
+        this.age = age;
+        this.color = color;
+    }
+public class Main {
+
+    public static void main(String[] args) {
+        public String getName();
+        {
+            return name;
+        }
+    }
+
+
+        // method 1
